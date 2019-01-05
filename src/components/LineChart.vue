3e19012e48aa649ab8f94cdb921ec010ab9a7e17
Wales Chang <pc963kimo@gmail.com>
2019-01-05 00:53:27 -0700

    feat: display movie information
    1. display vote average on the gauge as well as pictures
    2. load initial movie data from the backend server
    3. add card layout

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -2,23 +2,49 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h4>{{subtitle}}</h4>
-     <ul>
-       <li>
-         <div>
-           {{selectedMovie.title}}
-         </div>
-       </li>
-       <li>
-        <figure>
-        <chart :options="setMostPopOption" ref="bar" @mouseover="mouseOver" />
-        </figure>
-       </li>
-       <li>
-        <figure>
-        <chart :options="setOption" ref="bar" theme="ovilia-green" auto-resize/>
-        </figure>
-       </li>
-     </ul>
+    
+         <b-container fluid class="p-4">
+          <b-row>
+            <b-col>
+              <b-card v-bind:title= "selectedMovie.title"
+                    tag="article"
+                    style="max-width: 50rem;"
+                    class="mb-2">
+              <b-row>
+                <b-col>
+                  <b-img thumbnail fluid v-bind:src= "selectedMovie.poster_path" alt="Thumbnail" />
+                </b-col>
+                <b-col>
+                  <figure>
+                    <chart style="width:100%; height:100%" ref="gauge" :options="setVoteOption" />
+                  </figure>
+                  <p class="card-text">
+                    {{selectedMovie.overview}}
+                  </p>
+
+                </b-col>
+              </b-row>
+              <div slot="footer">
+                  <small class="text-muted">Release date: {{selectedMovie.releaseData}}</small>
+              </div>
+            </b-card>
+            </b-col>
+            <b-col>
+              <figure>
+              <chart :options="setMostPopOption" ref="bar" @mouseover="mouseOver" />
+              </figure>
+            </b-col>
+            
+          </b-row>
+          <b-row>
+            <b-col>
+              <figure>
+              <chart :options="setOption" ref="bar" theme="ovilia-green" auto-resize/>
+              </figure>
+            </b-col>
+          </b-row>
+        </b-container>
+     
   </div>
 </template>
 
@@ -28,7 +54,7 @@ import ECharts from 'vue-echarts/components/ECharts'
 import 'echarts/lib/component/title'
 import 'echarts/lib/chart/bar'
 import 'echarts/lib/component/tooltip'
-// import 'echarts/lib/chart/gauge'
+import 'echarts/lib/chart/gauge'
 export default {
   name: 'LineChart',
   components: {
@@ -38,8 +64,12 @@ export default {
     return {
       subtitle: 'Providing movies information for you',
       movieData: [],
+      baseImgUrl: "https://image.tmdb.org/t/p/w500/",
       selectedMovie: {
-        title: ""
+        title: "",
+        overview: "",
+        poster_path: "",
+        releaseData:""
       },
       setMostPopOption: {
         title: {
@@ -90,6 +120,37 @@ export default {
           data: [820, 932, 901, 934, 1290, 1330, 1300],
           type: 'bar'
         }]
+      },
+      setVoteOption: {
+        series: [
+          {
+            name: 'Vote avgerage',
+            type: 'gauge',
+            splitNumber: -1,
+            radius: '90%',
+            axisLine: {           
+              lineStyle: {    
+                 width: 10
+              }
+            },
+            axisLabel: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            pointer: {
+              show: false
+            },
+            lineStyle: {
+              "color": [
+                [0, "#1d60ac"],
+                [1, "#f6f7fa"]
+              ]
+            },
+            data: [{value: 0}]
+          }
+        ]
       }
 
     }
@@ -101,9 +162,31 @@ export default {
     this.getMoviesInfo();
   },
   methods: {
-   
     mouseOver: function(e) {
-      console.log(e)
+      this.updateMoiveCard(e.dataIndex);
+    },
+    updateMoiveCard(index) {
+      this.selectedMovie.title = this.movieData[this.movieData.length - index - 1].title;
+      this.selectedMovie.overview = this.movieData[this.movieData.length - index - 1].overview;
+      this.selectedMovie.poster_path = this.baseImgUrl + this.movieData[this.movieData.length - index - 1].poster_path;
+      this.selectedMovie.releaseData =  this.movieData[this.movieData.length - index - 1].release_date;
+      this.setVoteOption.series[0].data = [{value: this.movieData[this.movieData.length - index - 1].vote_avg * 10}];
+      this.setVoteOption.series[0].detail = {
+        formatter: function (value) {
+          return value + '%';
+        },
+        textStyle: {
+          "fontSize": "20",
+        }
+      }
+      this.setVoteOption.series[0].axisLine = {
+        lineStyle: {
+          "color": [
+            [this.movieData[this.movieData.length - index - 1].vote_avg / 10, "#1d60ac"],
+            [1, "#f6f7fa"]
+          ]
+        }
+      };
     },
     getMoviesInfo: function() {
       axios
@@ -112,13 +195,14 @@ export default {
         
         response => { 
           this.movieData = response.data;
+          this.updateMoiveCard(this.movieData.length-1);
           let movieName =  response.data.map(element => {
               return element.title;
           })
           let moviePop = response.data.map(element => {
             return element.pop;
           })
-
+         
           this.setMostPopOption.title.text = "Most popurlity movies in 2018"
           this.setMostPopOption.series = {
             data: moviePop.reverse(),
@@ -148,7 +232,6 @@ export default {
           },
           movieName = movieName.reverse().map(res => {
               return {
-                // value: res.substring(0,10) + "...",
                 value: res,
                 textStyle: {
                   align: 'right'
@@ -171,6 +254,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+img {
+  width: auto;
+  border-radius: 2px;
+  transition: width 1s;
+}
 .echarts {
   width: 500px;
   height: 500px;
